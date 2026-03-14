f6b35749de75ecce506d2bc0aabf8bb9179d83e3
kian-hidalgo-uf <kian.hidalgo@ufl.edu>
2026-03-14 15:38:25 -0400

    Initial project structure setup, added folders and well as header and source file templates

---
+++ src/HelperFuncts.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Kian Hidalgo on 3/14/26.
+//
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Kian Hidalgo on 3/14/26.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Fast Lookup Project Started!" << std::endl;
+    return 0;
+}
