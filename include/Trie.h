271e71468e3482f5ddbad3d1944cc674a0ccfc32
kian-hidalgo-uf <kian.hidalgo@ufl.edu>
2026-03-27 12:49:23 -0400

    Updated some comments

--- include/Trie.h
+++ include/Trie.h
@@ -10,31 +10,6 @@
 using namespace std;
 
 
-struct TrieNode {
-    TrieNode* children[26]; //array of 26 pointers, one for each lowercase
-    bool isEndOfWord; //marks if node is end of complete word
-    TrieNode();
-};
-
-class Trie {
-private:
-    TrieNode* root; //pointer to root
-    //helper to gather words below prefix
-    void collectWords(TrieNode* node, string currentWord, vector<string>& results) const;
-    //helper function that changes a words to lowercase because it is case sensitive
-    string normalizeWord(const string& word) const;
-
-public:
-    Trie();
-    ~Trie();
-    //inserts word into trie after normalizing it
-    void insert(const string& word);
-    //checks if word exisits in the trie
-    bool search(const string& word) const;
-    //returns all words with prefix
-    vector<string> prefixSearch(const string& prefix) const; //return all words with prefix
-    void clear(TrieNode* node);
-};
 
 
 
